028b0559d4c8d9301f0416525ce567923d4796d9
Alexandre do Valle <61100510+alexdovalle@users.noreply.github.com>
2022-05-04 16:31:23 -0400

    Delete head.js

--- Atividade 06/dist/builderComposite/classes/head.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Head = void 0;
-const abstract_part_1 = require("./abstract-part");
-class Head extends abstract_part_1.AbstractBodyParts {
-}
-exports.Head = Head;
-const head = new Head('KBT-11 : Missile', 32, 35);
-console.log(head);
