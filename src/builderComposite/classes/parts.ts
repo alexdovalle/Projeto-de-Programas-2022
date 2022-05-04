a512f50f0c67c951e54c43f4b57ce050c3d58029
Alexandre do Valle <61100510+alexdovalle@users.noreply.github.com>
2022-05-04 16:24:04 -0400

    Add files via upload
    Atividade 06 de PP-2022

---
+++ src/builderComposite/classes/parts.ts
@@ -0,0 +1,7 @@
+import { AbstractBodyParts } from "./abstract-part";
+
+
+export class Head extends AbstractBodyParts{}
+export class RightArm extends AbstractBodyParts{}
+export class LeftArm extends AbstractBodyParts{}
+export class Legs extends AbstractBodyParts{}
